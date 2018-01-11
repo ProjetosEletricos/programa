c3bcb67fd1aba8c22087cd5b57143730335f8f28
chris <chris@Inspiron>
2018-01-11 16:46:58 -0200

    aba quadro

---
+++ Eletrica2/src/programa/janelas/projeto/ProjetoChangeListener.java
@@ -0,0 +1,37 @@
+package programa.janelas.projeto;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import programa.janelas.fonte.extras.FonteApagarDados;
+import programa.janelas.quadro.extras.QuadroApagarDados;
+
+public class ProjetoChangeListener implements ChangeListener {
+
+	private ProjetoFrm frm;
+
+	public ProjetoChangeListener(ProjetoFrm frm) {
+
+		this.frm = frm;
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+
+		if (frm.getAbas().getSelectedComponent() == frm.getPanelFonte()) {
+
+			frm.iniciaTabelaFontes();
+			FonteApagarDados.formu(frm);
+
+		}
+		if (frm.getAbas().getSelectedComponent() == frm.getPanelQuadro()) {
+
+			frm.iniciaTabelaQuadros();
+			
+			QuadroApagarDados.formu(frm);
+
+		}
+
+	}
+
+}
